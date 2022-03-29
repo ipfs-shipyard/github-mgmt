3ee765c9dd1df0adeb1bece9a98f4700b9ab00e3
galargh <piotr.galar@gmail.com>
2022-03-29 11:17:27 +0200

    Configured github-mgmt

--- terraform/terraform_override.tf
+++ terraform/terraform_override.tf
@@ -1,6 +1,7 @@
 terraform {
   backend "s3" {
-    region               = "us-west-2"
+    # account_id = "642361402189"
+    region               = "us-east-1"
     bucket               = "github-mgmt"
     key                  = "terraform.tfstate"
     workspace_key_prefix = "org"
