097145cd10175ddf4246f3539338d87a486eecbf
Piotr Galar <piotr.galar@gmail.com>
2022-07-06 12:01:38 +0200

    Update resources_override.tf

--- terraform/resources_override.tf
+++ terraform/resources_override.tf
@@ -50,9 +50,6 @@ resource "github_branch_protection" "this" {
 resource "github_repository_file" "this" {
   lifecycle {
     ignore_changes = [
-      commit_author,
-      commit_email,
-      commit_message,
       overwrite_on_create,
     ]
   }
