e6678780d0cb4d509bf7b38f31dfd3e8368de3d3
Piotr Galar <piotr.galar@gmail.com>
2023-05-26 14:16:45 +0200

    Update fix-yaml-config.ts

--- scripts/src/actions/fix-yaml-config.ts
+++ scripts/src/actions/fix-yaml-config.ts
@@ -1,4 +1,20 @@
 import 'reflect-metadata'
+import {Repository} from '../resources/repository'
 import {format} from './shared/format'
+import {setPropertyInAllRepos} from './shared/set-property-in-all-repos'
 
+function isPublic(repository: Repository) {
+  return repository.visibility === 'public'
+}
+
+setPropertyInAllRepos(
+  'secret_scanning',
+  true,
+  r => isPublic(r)
+)
+setPropertyInAllRepos(
+  'secret_scanning_push_protection',
+  true,
+  r => isPublic(r)
+)
 format()
